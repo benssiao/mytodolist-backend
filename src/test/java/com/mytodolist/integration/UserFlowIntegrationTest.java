fb860c2d6c8abdb6d8666b6d4701914f8c1f4e1b
Ben Xiao <benssiao@gmail.com>
2025-10-19 13:56:17 -0700

    finished user controller integration testing

---
+++ src/test/java/com/mytodolist/integration/UserFlowIntegrationTest.java
@@ -0,0 +1,92 @@
+package com.mytodolist.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.mytodolist.security.dtos.RegisterRequestDTO;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Rollback
+
+public class UserFlowIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private String accessToken;
+
+    private String testUsername;
+
+    @BeforeEach
+    void setupEach() throws Exception {
+        testUsername = "testuser" + System.currentTimeMillis();
+        RegisterRequestDTO register = new RegisterRequestDTO(testUsername, "Password1");
+        String testUserandPassword = objectMapper.writeValueAsString(register);
+
+        mockMvc.perform(post("/api/v1/auth/register")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(testUserandPassword))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        MvcResult loginResult = mockMvc.perform(post("/api/v1/auth/login")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(testUserandPassword))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").exists())
+                .andReturn();
+
+        accessToken = JsonPath.read(loginResult.getResponse().getContentAsString(), "$.accessToken");
+
+    }
+
+    @Test
+    void tesGetUsers_Returns200() throws Exception {
+        mockMvc.perform(get("/api/v1/users/me")
+                .with(csrf())
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value(testUsername));
+    }
+
+    @Test
+    void testGetUsers_Unauthorized() throws Exception {
+        String invalidToken = accessToken + "invalid";
+        mockMvc.perform(get("/api/v1/users/me")
+                .with(csrf())
+                .header("Authorization", "Bearer " + invalidToken)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void testGetUsers_ForbiddenWithoutToken() throws Exception {
+        mockMvc.perform(get("/api/v1/users/me")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+}
